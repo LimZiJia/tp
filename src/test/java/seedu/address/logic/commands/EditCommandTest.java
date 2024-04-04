5803db2e5f397d1eb64a6dc7a343ff8f2a223671
gosongying <gosongying7@gmail.com>
2024-04-05 02:10:46 +0800

    Fix test cases compilation error

--- src/test/java/seedu/address/logic/commands/EditCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditCommandTest.java
@@ -10,8 +10,6 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.CommandTestUtil.showHousekeeperAtIndex;
-import static seedu.address.logic.commands.CommandTestUtil.showClientAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
@@ -25,12 +23,8 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.person.Client;
-import seedu.address.model.person.Housekeeper;
 import seedu.address.model.person.Person;
-import seedu.address.testutil.ClientBuilder;
 import seedu.address.testutil.EditPersonDescriptorBuilder;
-import seedu.address.testutil.HousekeeperBuilder;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for EditCommand.
@@ -41,76 +35,45 @@ public class EditCommandTest {
 
     @Test
     public void execute_allFieldsSpecifiedUnfilteredList_success() {
-        Client editedClient = new ClientBuilder().build();
-        Housekeeper editedHousekeeper = new HousekeeperBuilder().build();
-        EditPersonDescriptor descriptorClient = new EditPersonDescriptorBuilder(editedClient).build();
-        EditPersonDescriptor descriptorHousekeeper = new EditPersonDescriptorBuilder(editedHousekeeper).build();
-        EditCommand editClientCommand = new EditClientCommand(INDEX_FIRST_PERSON, descriptorClient);
-        EditCommand editHousekeeperCommand = new EditHousekeeperCommand(INDEX_FIRST_PERSON, descriptorHousekeeper);
+        Person editedPerson = new PersonBuilder().build();
+        EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(editedPerson).build();
+        EditCommand editCommand = new EditCommand(INDEX_FIRST_PERSON, descriptor);
 
-        String expectedMessageClient =
-                String.format(EditClientCommand.MESSAGE_EDIT_CLIENT_SUCCESS, Messages.formatClient(editedClient));
-        String expectedMessageHousekeeper =
-                String.format(EditHousekeeperCommand.MESSAGE_EDIT_HOUSEKEEPER_SUCCESS,
-                        Messages.formatHousekeeper(editedHousekeeper));
+        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_PERSON_SUCCESS, Messages.format(editedPerson));
 
-        Model expectedModelClient = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
-        expectedModelClient.setClient(model.getFilteredClientList().get(0), editedClient);
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.setPerson(model.getFilteredPersonList().get(0), editedPerson);
 
-        Model expectedModelHousekeeper = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
-        expectedModelHousekeeper.setHousekeeper(model.getFilteredHousekeeperList().get(0), editedHousekeeper);
-
-
-        assertCommandSuccess(editClientCommand, model, expectedMessageClient, expectedModelClient);
-
-        assertCommandSuccess(editClientCommand, model, expectedMessageHousekeeper, expectedModelHousekeeper);
+        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
     }
 
     @Test
     public void execute_someFieldsSpecifiedUnfilteredList_success() {
-        Index indexLastClient = Index.fromOneBased(model.getFilteredClientList().size());
-        Client lastClient = model.getFilteredClientList().get(indexLastClient.getZeroBased());
+        Index indexLastPerson = Index.fromOneBased(model.getFilteredPersonList().size());
+        Person lastPerson = model.getFilteredPersonList().get(indexLastPerson.getZeroBased());
 
-        Index indexLastHousekeeper = Index.fromOneBased(model.getFilteredHousekeeperList().size());
-        Housekeeper lastHousekeeper = model.getFilteredHousekeeperList().get(indexLastHousekeeper.getZeroBased());
-
-        ClientBuilder clientInList = new ClientBuilder(lastClient);
-        Client editedClient = clientInList.withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
+        PersonBuilder personInList = new PersonBuilder(lastPerson);
+        Person editedPerson = personInList.withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
                 .withTags(VALID_TAG_HUSBAND).build();
 
-        HousekeeperBuilder housekeeperInList = new HousekeeperBuilder(lastHousekeeper);
-        Housekeeper editedHousekeeper = housekeeperInList.withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
-                .withTags(VALID_TAG_HUSBAND).build();
-
-        EditPersonDescriptor descriptorC = new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB)
+        EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB)
                 .withPhone(VALID_PHONE_BOB).withTags(VALID_TAG_HUSBAND).build();
-        EditCommand editCommandClient = new EditClientCommand(indexLastClient, descriptorC);
+        EditCommand editCommand = new EditCommand(indexLastPerson, descriptor);
 
-        EditPersonDescriptor descriptorH = new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB)
-                .withPhone(VALID_PHONE_BOB).withTags(VALID_TAG_HUSBAND).build();
-        EditCommand editCommandHousekeeper = new EditHousekeeperCommand(indexLastHousekeeper, descriptorH);
+        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_PERSON_SUCCESS, Messages.format(editedPerson));
 
-        String expectedMessageH =
-                String.format(EditClientCommand.MESSAGE_EDIT_CLIENT_SUCCESS, Messages.formatClient(editedClient));
-        String expectedMessageC =
-                String.format(EditHousekeeperCommand.MESSAGE_EDIT_HOUSEKEEPER_SUCCESS, Messages.formatHousekeeper(editedHousekeeper));
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.setPerson(lastPerson, editedPerson);
 
-        Model expectedModelClient = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
-        expectedModelClient.setClient(lastClient, editedClient);
-        Model expectedModelHousekeeper = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
-        expectedModelHousekeeper.setHousekeeper(lastHousekeeper, editedHousekeeper);
-
-        assertCommandSuccess(editCommandClient, model, expectedMessageC, expectedModelClient);
-        assertCommandSuccess(editCommandHousekeeper, model, expectedMessageH, expectedModelHousekeeper);
+        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
     }
 
     @Test
     public void execute_noFieldSpecifiedUnfilteredList_success() {
-        EditCommand editCommand = new EditClientCommand(INDEX_FIRST_PERSON, new EditPersonDescriptor());
-        Client editedPerson = model.getFilteredClientList().get(INDEX_FIRST_PERSON.getZeroBased());
+        EditCommand editCommand = new EditCommand(INDEX_FIRST_PERSON, new EditPersonDescriptor());
+        Person editedPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
 
-        String expectedMessage =
-                String.format(EditClientCommand.MESSAGE_EDIT_CLIENT_SUCCESS, Messages.formatClient(editedPerson));
+        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_PERSON_SUCCESS, Messages.format(editedPerson));
 
         Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
 
@@ -119,48 +82,47 @@ public class EditCommandTest {
 
     @Test
     public void execute_filteredList_success() {
-        showClientAtIndex(model, INDEX_FIRST_PERSON);
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
 
-        Client personInFilteredList = model.getFilteredClientList().get(INDEX_FIRST_PERSON.getZeroBased());
-        Client editedPerson = new ClientBuilder(personInFilteredList).withName(VALID_NAME_BOB).build();
-        EditCommand editCommand = new EditClientCommand(INDEX_FIRST_PERSON,
+        Person personInFilteredList = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        Person editedPerson = new PersonBuilder(personInFilteredList).withName(VALID_NAME_BOB).build();
+        EditCommand editCommand = new EditCommand(INDEX_FIRST_PERSON,
                 new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB).build());
 
-        String expectedMessage =
-                String.format(EditClientCommand.MESSAGE_EDIT_CLIENT_SUCCESS, Messages.formatClient(editedPerson));
+        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_PERSON_SUCCESS, Messages.format(editedPerson));
 
         Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
-        expectedModel.setClient(model.getFilteredClientList().get(0), editedPerson);
+        expectedModel.setPerson(model.getFilteredPersonList().get(0), editedPerson);
 
         assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
     }
 
     @Test
     public void execute_duplicatePersonUnfilteredList_failure() {
-        Client firstPerson = model.getFilteredClientList().get(INDEX_FIRST_PERSON.getZeroBased());
+        Person firstPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
         EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(firstPerson).build();
-        EditCommand editCommand = new EditClientCommand(INDEX_SECOND_PERSON, descriptor);
+        EditCommand editCommand = new EditCommand(INDEX_SECOND_PERSON, descriptor);
 
-        assertCommandFailure(editCommand, model, EditClientCommand.MESSAGE_DUPLICATE_CLIENT);
+        assertCommandFailure(editCommand, model, EditCommand.MESSAGE_DUPLICATE_PERSON);
     }
 
     @Test
     public void execute_duplicatePersonFilteredList_failure() {
-        showClientAtIndex(model, INDEX_FIRST_PERSON);
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
 
         // edit person in filtered list into a duplicate in address book
-        Person personInList = model.getAddressBook().getClientList().get(INDEX_SECOND_PERSON.getZeroBased());
-        EditCommand editCommand = new EditClientCommand(INDEX_FIRST_PERSON,
+        Person personInList = model.getAddressBook().getPersonList().get(INDEX_SECOND_PERSON.getZeroBased());
+        EditCommand editCommand = new EditCommand(INDEX_FIRST_PERSON,
                 new EditPersonDescriptorBuilder(personInList).build());
 
-        assertCommandFailure(editCommand, model, EditClientCommand.MESSAGE_DUPLICATE_CLIENT);
+        assertCommandFailure(editCommand, model, EditCommand.MESSAGE_DUPLICATE_PERSON);
     }
 
     @Test
     public void execute_invalidPersonIndexUnfilteredList_failure() {
-        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredClientList().size() + 1);
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
         EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB).build();
-        EditCommand editCommand = new EditClientCommand(outOfBoundIndex, descriptor);
+        EditCommand editCommand = new EditCommand(outOfBoundIndex, descriptor);
 
         assertCommandFailure(editCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
     }
@@ -171,24 +133,24 @@ public class EditCommandTest {
      */
     @Test
     public void execute_invalidPersonIndexFilteredList_failure() {
-        showClientAtIndex(model, INDEX_FIRST_PERSON);
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
         Index outOfBoundIndex = INDEX_SECOND_PERSON;
         // ensures that outOfBoundIndex is still in bounds of address book list
-        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getClientList().size());
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getPersonList().size());
 
-        EditCommand editCommand = new EditClientCommand(outOfBoundIndex,
+        EditCommand editCommand = new EditCommand(outOfBoundIndex,
                 new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB).build());
 
-        assertCommandFailure(editCommand, model, Messages.MESSAGE_INVALID_CLIENT_DISPLAYED_INDEX);
+        assertCommandFailure(editCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
     }
 
     @Test
     public void equals() {
-        final EditCommand standardCommand = new EditClientCommand(INDEX_FIRST_PERSON, DESC_AMY);
+        final EditCommand standardCommand = new EditCommand(INDEX_FIRST_PERSON, DESC_AMY);
 
         // same values -> returns true
         EditPersonDescriptor copyDescriptor = new EditPersonDescriptor(DESC_AMY);
-        EditCommand commandWithSameValues = new EditClientCommand(INDEX_FIRST_PERSON, copyDescriptor);
+        EditCommand commandWithSameValues = new EditCommand(INDEX_FIRST_PERSON, copyDescriptor);
         assertTrue(standardCommand.equals(commandWithSameValues));
 
         // same object -> returns true
@@ -201,17 +163,17 @@ public class EditCommandTest {
         assertFalse(standardCommand.equals(new ClearCommand()));
 
         // different index -> returns false
-        assertFalse(standardCommand.equals(new EditClientCommand(INDEX_SECOND_PERSON, DESC_AMY)));
+        assertFalse(standardCommand.equals(new EditCommand(INDEX_SECOND_PERSON, DESC_AMY)));
 
         // different descriptor -> returns false
-        assertFalse(standardCommand.equals(new EditClientCommand(INDEX_FIRST_PERSON, DESC_BOB)));
+        assertFalse(standardCommand.equals(new EditCommand(INDEX_FIRST_PERSON, DESC_BOB)));
     }
 
     @Test
     public void toStringMethod() {
         Index index = Index.fromOneBased(1);
         EditPersonDescriptor editPersonDescriptor = new EditPersonDescriptor();
-        EditCommand editCommand = new EditClientCommand(index, editPersonDescriptor);
+        EditCommand editCommand = new EditCommand(index, editPersonDescriptor);
         String expected = EditCommand.class.getCanonicalName() + "{index=" + index + ", editPersonDescriptor="
                 + editPersonDescriptor + "}";
         assertEquals(expected, editCommand.toString());
--- src/test/java/seedu/address/model/person/LeadsFilterPredicateTest.java
+++ src/test/java/seedu/address/model/person/LeadsFilterPredicateTest.java
@@ -1,8 +1,6 @@
 package seedu.address.model.person;
 
 import org.junit.jupiter.api.Test;
-import seedu.address.testutil.ClientBuilder;
-import seedu.address.testutil.HousekeeperBuilder;
 
 import java.time.LocalDate;
 import java.time.Period;
@@ -10,44 +8,44 @@ import java.time.Period;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 class LeadsFilterPredicateTest {
-    // The combination here gives a predicted housekeeping date that is in the past. Client should get notification.
-    private static final LocalDate DEFAULT_DATE = LocalDate.parse("2023-04-01");
-    private static final Period DEFAULT_PERIOD = Period.ofMonths(1);
+        // The combination here gives a predicted housekeeping date that is in the past. Client should get notification.
+        private static final LocalDate DEFAULT_DATE = LocalDate.parse("2023-04-01");
+        private static final Period DEFAULT_PERIOD = Period.ofMonths(1);
 
-    @Test
-    void client_no_housekeeping_details() {
-        LeadsFilterPredicate predicate = new LeadsFilterPredicate();
+        @Test
+        void client_no_housekeeping_details() {
+            LeadsFilterPredicate predicate = new LeadsFilterPredicate();
 
-        HousekeepingDetails housekeepingDetails = new HousekeepingDetails();
-        ClientBuilder clientBuilder = new ClientBuilder().withDetails(housekeepingDetails);
-        Client client = clientBuilder.build();
+            HousekeepingDetails housekeepingDetails = new HousekeepingDetails();
+            PersonBuilder personBuilder = new PersonBuilder().withHousekeepingDetails(housekeepingDetails);
+            Client client = (Client) personBuilder.build();
 
-        assertFalse(predicate.test(client));
-    }
+            assertFalse(predicate.test(client));
+        }
 
-    @Test
-    void client_no_booking_date() {
-        LeadsFilterPredicate predicate = new LeadsFilterPredicate();
+        @Test
+        void client_no_booking_date() {
+            LeadsFilterPredicate predicate = new LeadsFilterPredicate();
 
-        HousekeepingDetails housekeepingDetails = new HousekeepingDetails(DEFAULT_DATE, DEFAULT_PERIOD);
-        ClientBuilder clientBuilder = new ClientBuilder().withDetails(housekeepingDetails);
-        Client client = clientBuilder.build();
+            HousekeepingDetails housekeepingDetails = new HousekeepingDetails(DEFAULT_DATE, DEFAULT_PERIOD);
+            PersonBuilder personBuilder = new PersonBuilder().withHousekeepingDetails(housekeepingDetails);
+            Client client = (Client) personBuilder.build();
 
-        assertFalse(predicate.test(client));
-    }
+            assertFalse(predicate.test(client));
+        }
 
-    @Test
-    void client_booking_date_after_today() {
-        LeadsFilterPredicate predicate = new LeadsFilterPredicate();
+        @Test
+        void client_booking_date_after_today() {
+            LeadsFilterPredicate predicate = new LeadsFilterPredicate();
 
-        Period period = Period.ofMonths(1);
-        LocalDate date = LocalDate.now().plus(period); // Create a date one month from now
-        HousekeepingDetails housekeepingDetails = new HousekeepingDetails(date, DEFAULT_PERIOD);
-        ClientBuilder clientBuilder = new ClientBuilder().withDetails(housekeepingDetails);
-        Client client = clientBuilder.build();
+            Period period = Period.ofMonths(1);
+            LocalDate date = LocalDate.now().plus(period); // Create a date one month from now
+            HousekeepingDetails housekeepingDetails = new HousekeepingDetails(date, DEFAULT_PERIOD);
+            PersonBuilder personBuilder = new PersonBuilder().withHousekeepingDetails(housekeepingDetails);
+            Client client = (Client) personBuilder.build();
 
-        assertFalse(predicate.test(client));
-    }
+            assertFalse(predicate.test(client));
+        }
 
     @Test
     void client_booking_date_today() {
@@ -55,8 +53,8 @@ class LeadsFilterPredicateTest {
 
         LocalDate date = LocalDate.now(); // Create a date that is today
         HousekeepingDetails housekeepingDetails = new HousekeepingDetails(date, DEFAULT_PERIOD);
-        ClientBuilder clientBuilder = new ClientBuilder().withDetails(housekeepingDetails);
-        Client client = clientBuilder.build();
+        PersonBuilder personBuilder = new PersonBuilder().withHousekeepingDetails(housekeepingDetails);
+        Client client = (Client) personBuilder.build();
 
         assertFalse(predicate.test(client));
     }
@@ -68,8 +66,8 @@ class LeadsFilterPredicateTest {
         Period period = Period.ofMonths(1);
         LocalDate date = LocalDate.now().minus(period); // Create a date that is before today
         HousekeepingDetails housekeepingDetails = new HousekeepingDetails(date, DEFAULT_PERIOD);
-        ClientBuilder clientBuilder = new ClientBuilder().withDetails(housekeepingDetails);
-        Client client = clientBuilder.build();
+        PersonBuilder personBuilder = new PersonBuilder().withHousekeepingDetails(housekeepingDetails);
+        Client client = (Client) personBuilder.build();
 
         assertTrue(predicate.test(client));
     }
@@ -79,8 +77,8 @@ class LeadsFilterPredicateTest {
         LeadsFilterPredicate predicate = new LeadsFilterPredicate();
 
         HousekeepingDetails housekeepingDetails = new HousekeepingDetails(DEFAULT_DATE, DEFAULT_PERIOD);
-        ClientBuilder clientBuilder = new ClientBuilder().withDetails(housekeepingDetails);
-        Client client = clientBuilder.build();
+        PersonBuilder personBuilder = new PersonBuilder().withHousekeepingDetails(housekeepingDetails);
+        Client client = (Client) personBuilder.build();
 
         assertTrue(predicate.test(client));
     }
@@ -92,8 +90,8 @@ class LeadsFilterPredicateTest {
         LocalDate date = LocalDate.now();
         Period period = Period.ZERO;
         HousekeepingDetails housekeepingDetails = new HousekeepingDetails(date, period);
-        ClientBuilder clientBuilder = new ClientBuilder().withDetails(housekeepingDetails);
-        Client client = clientBuilder.build();
+        PersonBuilder personBuilder = new PersonBuilder().withHousekeepingDetails(housekeepingDetails);
+        Client client = (Client) personBuilder.build();
 
         assertTrue(predicate.test(client));
     }
@@ -105,8 +103,8 @@ class LeadsFilterPredicateTest {
         LocalDate date = LocalDate.now();
         Period period = Period.ofMonths(1);
         HousekeepingDetails housekeepingDetails = new HousekeepingDetails(date, period);
-        ClientBuilder clientBuilder = new ClientBuilder().withDetails(housekeepingDetails);
-        Client client = clientBuilder.build();
+        PersonBuilder personBuilder = new PersonBuilder().withHousekeepingDetails(housekeepingDetails);
+        Client client = (Client) personBuilder.build();
 
         assertFalse(predicate.test(client));
     }
